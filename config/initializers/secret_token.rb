2892d7bd71eb287b176129dd6d4fc33ee702d027
CesarConopoima <cesar.conopoima@gmail.com>
2014-11-29 22:24:15 -0430

    release

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Americanparts::Application.config.secret_token = '6c9909f0197c705ee29121e2b5ec1405437b50ee09c0f70fa519064f3406dda91d958d90a369188f6bef121f0fa2ed04bf15ea4ce7f7d26619cb699509ea0927'
