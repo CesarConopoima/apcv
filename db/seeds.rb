c2f9f7089134705a1a455a3eed833bffca968747
CesarConopoima <cesar.conopoima@gmail.com>
2014-12-01 12:33:47 -0430

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all															
+Product.create(name:'BIELA CORTA MOD 06E',code:'BIE-1212',marc:'Copeland',quantity:100,price:2.0,imageurl:'logo/BIE-1212.gif',model:'06E',flagmis:'no')
+Product.create(name:'BIELA LARGA MOD 07D',code:'BIE-1213',marc:'Carrier',quantity:100,price:2.0,imageurl:'logo/BIE-1213.gif',model:'07D',flagmis:'no')
+Product.create(name:'BIELA LARGA MOD 06D',code:'BIE-1214',marc:'Bitzer',quantity:100,price:2.0,imageurl:'logo/BIE-1214.gif',model:'06D',flagmis:'no')
