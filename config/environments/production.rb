7980960eaaef13f23a2c7a8f6181f6566a967cd4
CesarConopoima <cesar.conopoima@gmail.com>
2014-12-04 15:05:34 -0430

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Americanparts::Application.configure do
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            'coonopoima.cesar@gmail.com',
-  password:              ENV['PASS'],
+  user_name:            'cesar.conopoima@gmail.com',
+  password:              ENV['KEY_2'],
   authentication:       'plain',
   enable_starttls_auto: true  }
 
