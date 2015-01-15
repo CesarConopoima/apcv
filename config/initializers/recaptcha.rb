10b2960ede38b18c303ad90414f2a5a3df95023b
CesarConopoima <cesar.conopoima@gmail.com>
2015-01-15 09:48:03 -0430

    changeTolog

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,9 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6LfobQATAAAAAK5MSRwagq5H6nMYfjWQcyHyV1Bb'
-  config.private_key = '6LfobQATAAAAA'
+  # config.public_key  = '6LfobQATAAAAAK5MSRwagq5H6nMYfjWQcyHyV1Bb'
+  # config.private_key = '6LfobQATAAAAA'
+
+  config.public_key  = ENV['GOOGLE_SITE_KEY_1']
+  config.private_key = ENV['GOOGLE_SECRET_KEY']
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
   # Uncomment if you want to use the newer version of the API,
