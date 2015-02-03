8339fb5f2b6089a207eb6bb8c1317782ede140ed
CesarConopoima <cesar.conopoima@gmail.com>
2015-02-02 21:54:09 -0430

    mailer

--- app/mailers/promotion_mailer.rb
+++ app/mailers/promotion_mailer.rb
@@ -4,7 +4,8 @@ class PromotionMailer < ActionMailer::Base
   def promotion(imgs)
   	#normally products should come as string to make db call smt like 
   	#beware to send mail only to users that has accepted to receive it
-  	attachments.inline['image.jpg'] = File.read(imgs)
+  	#@rootURL = request.host
+  	attachments.inline['image.jpg'] = File.read(Rails.root.join(imgs))
 
   	mail(to: "cesar.conopoima@gmail.com", subject: "This are promotions from this month")
   end
